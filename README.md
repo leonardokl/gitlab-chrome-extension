9322899caa23b6731d50644378fecd33ef79d3e0
Leonardo Luiz <leonardokl@hotmail.com>
2016-09-15 22:51:31 -0300

    Fix error when new branch button is not hovered

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Gitlab Chrome Extension
 
 Copy Gitlab new branch name to clipboard
+
+![alt text](src/images/screenshot.png)
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,50 @@
+/* global chrome */
+
+var Utils = {
+	responseIsValid: function (response) {
+		return !!(response && response[0] && response[0] !== null);
+	}
+}
+
+/* Chrome API calls returning promises */
 var ChromeAPI = {
-	fetchBranchName: function (tab, callback) {
-		chrome.tabs.executeScript(tab, {
-			code: 'document.getElementById("new-branch").getElementsByTagName("a")[0].getAttribute("data-original-title");'
-		}, callback);
+	query: function (options) {
+		return new Promise(function (resolve, reject) {
+			chrome.tabs.query(options,
+				function (response) {
+					if (!Utils.responseIsValid(response)) {
+						return reject(response)
+					}
+
+					resolve(response[0]);
+				});
+		});
 	},
-	getCurrentTab: function (callback) {
-		chrome.tabs.query({active: true}, callback);
+
+	executeScript: function (tabId, options) {
+		return new Promise(function (resolve, reject) {
+			chrome.tabs.executeScript(tabId, options, function (response) {
+				if (!Utils.responseIsValid(response)) {
+					return reject(response);
+				}
+
+				resolve(response[0]);
+			});
+		})
+	}
+};
+
+var API = {
+	chrome: ChromeAPI,
+
+	getCurrentTab: function () {
+		return API.chrome.query({active: true});
+	},
+
+	getNewBranchButtonAttribute: function (tabId, attribute) {
+		return API.chrome.executeScript(tabId, {
+			code: 'document.getElementById("new-branch").getElementsByTagName("a")[1].getAttribute("' + attribute  + '");',
+		});
 	}
 };
 
@@ -15,11 +54,51 @@ var DOM = {
 	}
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	ChromeAPI.getCurrentTab(function (tab) {
-		ChromeAPI.fetchBranchName(tab[0].id, function (response) {
-			DOM.setBranchName(response);
-			new Clipboard('.btn');
+var Action = {
+	addClipboardListener: function () {
+		new Clipboard('.btn');
+	},
+
+	setBrancheNameAndAddClipboardListener: function (name) {
+		DOM.setBranchName('git checkout -b ' + name);
+		Action.addClipboardListener();
+	},
+
+	fetchBranchNameAttribute: function (tabId) {
+		return new Promise(function (resolve, reject) {
+			API.getNewBranchButtonAttribute(tabId, "title")
+				.then(function (response) {
+					resolve(response);
+				})
+				.catch(function () {
+					API.getNewBranchButtonAttribute(tabId, "data-original-title")
+						.then(function (response) {
+							resolve(response);
+						})
+						.catch(function () {
+							reject();
+						});
+				});
+		})
+	},
+
+	fetchBranchName: function () {
+		API.getCurrentTab()
+			.then(function (tab) {
+				Action.fetchBranchNameAttribute()
+					.then(function (response) {
+						Action.setBrancheNameAndAddClipboardListener(response);
+					})
+					.catch(function (error) {
+						DOM.setBranchName('Branch name not found');
+					});
 		});
-	});
-});
+	}
+};
+
+var initApp = function () {
+	Action.fetchBranchName();
+};
+
+// EVENT LISTENERS
+document.addEventListener('DOMContentLoaded', initApp);
